4ab12b3a3b4d5a475370f353ed2a1e6a3fa6106f
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-01-02 12:58:29 -0300

    Reformulação da questão 1 para uso com string

--- Unidade01-Avaliacao02/q1.py
+++ Unidade01-Avaliacao02/q1.py
@@ -1,18 +1,23 @@
-print("CONTADOR DE NÚMEROS DECRESCENTES ENTRE 10 E 987631")
+print("CONTADOR DE NÚMEROS DECRESCENTES ENTRE 10 E 987631\n")
 
 N_INICIO = 10
 N_FIM = 987631
 
-DIV = 10
+N_INICIO_INTERACAO = N_INICIO
 
-while N_INICIO <= N_FIM:
+CONT = 0
+
+while N_INICIO_INTERACAO <= N_FIM:
     VER = True
-    NUMERO = N_INICIo
     
-    while VER:
+    for n in range(0, len(str(N_INICIO_INTERACAO)) - 1):
+        if str(N_INICIO_INTERACAO)[n] < str(N_INICIO_INTERACAO)[n + 1]:
+            VER = False
+
+    if VER:
+        CONT += 1
+        print(N_INICIO_INTERACAO)
         
-    print(N_INICIO)
+    N_INICIO_INTERACAO += 1
 
-    N_INICIO += 1
-
-    if DIV *= 10
+print("\nEntre",N_INICIO,"e",N_FIM,"existem",CONT,"números decrescentes")
