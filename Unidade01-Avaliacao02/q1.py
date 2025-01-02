bc442e2f95a659539f78d5136cfac1ad14286262
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-01-02 09:00:48 -0300

    Atualizando questão 1

--- Unidade01-Avaliacao02/q1.py
+++ Unidade01-Avaliacao02/q1.py
@@ -1,1 +1,18 @@
 print("CONTADOR DE NÚMEROS DECRESCENTES ENTRE 10 E 987631")
+
+N_INICIO = 10
+N_FIM = 987631
+
+DIV = 10
+
+while N_INICIO <= N_FIM:
+    VER = True
+    NUMERO = N_INICIo
+    
+    while VER:
+        
+    print(N_INICIO)
+
+    N_INICIO += 1
+
+    if DIV *= 10
