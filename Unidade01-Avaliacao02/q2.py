30b137bd4f9b988020914dafd97f2ceb8992933e
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-01-03 09:00:12 -0300

    Início da questão 2

---
+++ Unidade01-Avaliacao02/q2.py
@@ -0,0 +1,14 @@
+print("VERIFICANDO SE NÚMERO É PALÍNDROMO")
+print("OBS: informe 0 (zero) para sair\n")
+
+N = 1
+
+while N != 0:
+    try:
+        N = int(input("Informe um número: "))
+
+        
+
+    except:
+        print("\n[ERRO] - VALOR INCORRETO INFORMADO. Tente novamente.\n")
+        print("------------------------------------------------\n")
