494c89f47a256300da9713dd48f5261ba29a07ae
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-01-04 09:09:07 -0300

    Atualizando questão 2, contando algarismos do número informado

--- Unidade01-Avaliacao02/q2.py
+++ Unidade01-Avaliacao02/q2.py
@@ -7,8 +7,45 @@ while N != 0:
     try:
         N = int(input("Informe um número: "))
 
-        
+        VER = True #verificador para parar laço interação com o número informado
+        DIV = 10 #divisor, para separar cada algarismo, iniciando com 10
+        ALG_CONT = 1 #quantidade de algarismos que o número possui
 
-    except:
-        print("\n[ERRO] - VALOR INCORRETO INFORMADO. Tente novamente.\n")
+        #verificando a quantidade de algarismos que o número possui
+        while VER:
+            if N // DIV != 0:
+                ALG_CONT += 1
+                
+                DIV *= 10
+            else:
+                VER = False
+
+        VER = True
+        ALG = [None] * ALG_CONT #lista para guardar cada algarismo do número
+        DIV = DIV**ALG_CONT
+        
+        for i in range(ALG_CONT, 0, -1):
+            print(i)
+
+        """
+                       
+            if DIV == 10 and len(ALG) == ALG_CONT - 1:
+                ALG.append(N % DIV)
+            elif DIV == 10:
+                ALG.append(N // DIV)
+                DIV *= 10
+            else:
+                if N // DIV != 0:
+                    ALG.append(N // DIV)
+                    DIV *= 10
+                else:
+                    VER = False
+        """
+
+        print("\nLista formada",ALG)
+        print("------------------------------------------------\n")
+            
+
+    except Exception as e:
+        print("\n[ERRO] - VALOR INCORRETO INFORMADO. Tente novamente.\n", e)
         print("------------------------------------------------\n")
