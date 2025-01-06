505ae43736e0dbae2582f73f5a9b701546c3d1f2
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-01-06 08:58:14 -0300

    Primeiras linhas da questão 4

---
+++ Unidade01-Avaliacao02/q4.py
@@ -0,0 +1,45 @@
+import random
+
+print("T-E-R-M-O\n")
+
+palavras = (
+    "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
+    "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
+    "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
+    "BURRO", "CAIXA", "CALDO", "CANJA", "CARRO", "CARTA", "CERVO",
+    "CESTA", "CLIMA", "COBRA", "COLAR", "COQUE", "COURO", "CRAVO",
+    "DARDO", "FAIXA", "FARDO", "FENDA", "FERRO", "FESTA", "FLUOR",
+    "FORCA", "FORNO", "FORTE", "FUNDO", "GAITA", "GARRA", "GENIO",
+    "GESSO", "GRADE", "GRANA", "GRAMA", "GURIA", "GREVE", "GRUTA",
+    "HEROI", "HOTEL", "ICONE", "IMPAR", "IMUNE", "INDIO", "JUNTA",
+    "LAPIS", "LARVA", "LAZER", "LENTO", "LESTE", "LIMPO", "LIVRO",
+    "MACIO", "MAGRO", "MALHA", "MANSO", "MARCO", "METAL", "MORTE",
+    "MORRO", "MURAL", "MOVEL", "NACAO", "NINHO", "NOBRE", "NORMA",
+    "NORTE", "NUVEM", "PACTO", "PALHA", "PARDO", "PARTE", "PEDRA",
+    "PEDAL", "PEIXE", "PRADO", "PISTA", "POMBO", "POETA", "PONTO",
+    "PRATO", "PRECO", "PRESO", "PROSA", "PRUMO", "PULGA", "PULSO",
+    "QUEPE", "RAIVA", "RISCO", "RITMO", "ROSTO", "ROUPA", "SABAO",
+    "SALTO", "SENSO", "SINAL", "SITIO", "SONHO", "SOPRO", "SURDO",
+    "TARDE", "TERNO", "TERMO", "TERRA", "TIGRE", "TINTA", "TOLDO",
+    "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
+    "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
+)
+
+TENT = -1
+
+#definindo as duas palavras de forma aleatória 
+PALAVRA_1 = random.choice(palavras)
+PALAVRA_2 = random.choice(palavras)
+
+#lista com números ordinais, para uma melhor organização das tentativas
+TENTATIVAS = ["PRIMEIRA","SEGUNDA","TERCEIRA","QUARTA","QUINTA","SEXTA","SÉTIMA"]
+
+#laço que garante a distinção entre as palavras 1 e 2
+while PALAVRA_1 == PALAVRA_2:
+    PALAVRA_2 = random.choice(palavras)
+
+#laço das 7 tentativas possíveis para encontrar as duas palavras
+while TENT < 6:
+    TENT += 1
+    print(TENTATIVAS[TENT],PALAVRA_1, PALAVRA_2)
+    
